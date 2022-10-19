df75572de11c1e572405988c35d90ca2f66082a7
Denys Chornokon <denyschornokon3@gmail.com>
2022-10-19 22:25:40 +0300

    no docker yet

--- lab5/public/main.js
+++ lab5/public/main.js
@@ -14,3 +14,36 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
+
+// !fetch
+
+function getPosts() {
+    fetch('http://localhost:3000/laptops')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = `<div class="heading"><h2>Buy a laptop</h2></div>`;
+            data.forEach(function (laptops) {
+                output += `
+              <div class="box">
+                <h3>${laptops.title}</h3>
+                <div class="content">
+                    <span>${laptops.id}</span>
+                    <span>${laptops.hdd}</span>
+                    <a href="${laptops.link}">Buy</a>
+
+                </div>
+              </div>
+              `;
+            });
+            document.getElementById('output').innerHTML = output;
+        })
+}
+
+getPosts()
+
+// const image = document.getElementById('pic');
+// function setSrc(){
+//     image.src="./img/1.jpg"
+// }
+
+// setSrc()
